86c2e868ec1bcf486424c8e87ceb1e66d9bf4efe
Matt Lord <mattalord@gmail.com>
2022-07-07 13:52:32 -0400

    Delete all legacy sharding related code (#10278)
    * Delete all legacy sharding related code
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Move used until functions from initialsharding to cluster
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Remove vtctl commands
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Kill vtworker and SetKeyspaceServedFrom cmd
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * WaitForDrain related stragglers
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Legacy local straggler workflow
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Get rid of SetKeyspaceShardingInfo & wait for drain stragglers
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Remove vtworker stragglers
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Update throttlerservice protobuf
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Rename test 24, add hashicorp vault test to it (now mysql_server_vault)
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Remove last mentiones of legacy sharding in vtctl
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * remove binlog_use_v3_resharding_mode
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Address review comments
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>
    
    * Correct vtgate help output
    
    Signed-off-by: Matt Lord <mattalord@gmail.com>

--- go/test/endtoend/backup/vtctlbackup/backup_utils.go
+++ go/test/endtoend/backup/vtctlbackup/backup_utils.go
@@ -28,8 +28,6 @@ import (
 	"testing"
 	"time"
 
-	"vitess.io/vitess/go/test/endtoend/sharding/initialsharding"
-
 	"vitess.io/vitess/go/vt/mysqlctl"
 	"vitess.io/vitess/go/vt/proto/topodata"
 
@@ -104,11 +102,11 @@ func LaunchCluster(setupType int, streamMode string, stripes int) (int, error) {
 	}
 	shard := &localCluster.Keyspaces[0].Shards[0]
 
-	dbCredentialFile = initialsharding.WriteDbCredentialToTmp(localCluster.TmpDirectory)
+	dbCredentialFile = cluster.WriteDbCredentialToTmp(localCluster.TmpDirectory)
 	initDb, _ := os.ReadFile(path.Join(os.Getenv("VTROOT"), "/config/init_db.sql"))
 	sql := string(initDb)
 	newInitDBFile = path.Join(localCluster.TmpDirectory, "init_db_with_passwords.sql")
-	sql = sql + initialsharding.GetPasswordUpdateSQL()
+	sql = sql + cluster.GetPasswordUpdateSQL(localCluster)
 	err = os.WriteFile(newInitDBFile, []byte(sql), 0666)
 	if err != nil {
 		return 1, err
