cc61316e84a2142a1ad424fac9c9edac2a674c0c
Manan Gupta <35839558+GuptaManan100@users.noreply.github.com>
2023-01-10 16:09:14 +0530

    VTOrc running PRS when database_instance empty bug fix. (#12019)
    * feat: convert join with database_instance to a left join and prevent fixes from running if the information from database_instance is unavailable
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    * test: add tests to verify the fix works
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>
    
    Signed-off-by: Manan Gupta <manan@planetscale.com>

--- go/vt/vtorc/inst/analysis_dao.go
+++ go/vt/vtorc/inst/analysis_dao.go
@@ -79,6 +79,7 @@ func GetReplicationAnalysis(clusterName string, hints *ReplicationAnalysisHints)
 		vitess_keyspace.keyspace_type AS keyspace_type,
 		vitess_keyspace.durability_policy AS durability_policy,
 		primary_instance.read_only AS read_only,
+		MIN(primary_instance.hostname) IS NULL AS is_invalid,
 		MIN(primary_instance.data_center) AS data_center,
 		MIN(primary_instance.region) AS region,
 		MIN(primary_instance.physical_environment) AS physical_environment,
@@ -294,7 +295,7 @@ func GetReplicationAnalysis(clusterName string, hints *ReplicationAnalysisHints)
 		JOIN vitess_keyspace ON (
 			vitess_tablet.keyspace = vitess_keyspace.keyspace
 		)
-		JOIN database_instance primary_instance ON (
+		LEFT JOIN database_instance primary_instance ON (
 			vitess_tablet.hostname = primary_instance.hostname
 			AND vitess_tablet.port = primary_instance.port
 		)
@@ -473,6 +474,10 @@ func GetReplicationAnalysis(clusterName string, hints *ReplicationAnalysisHints)
 		}
 		if ca.durability == nil {
 			// We failed to load the durability policy, so we shouldn't run any analysis
+			return nil
+		}
+		isInvalid := m.GetBool("is_invalid")
+		if isInvalid {
 			return nil
 		}
 		if a.IsClusterPrimary && !a.LastCheckValid && a.CountReplicas == 0 {
