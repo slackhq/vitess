5e1e611a5ceeee4f47cbcc0f3cf6989870f35d51
Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
2023-03-14 16:09:08 +0100

    Emit per workload labels for existing per table vttablet metrics (#12394)
    * Emit per workload labels for existing per table vttablet metrics
    
    This adds the possibility to configure vttablet (via CLI flag) to also have a
    workload label for existing per table metrics (query counts, query times, query
    errors, query rows affected, query rows returned, query error counts). Workload
    can be any string that makes sense for the client application. For example, API
    endpoint name, controller, batch job name, application name or something else.
    
    This is usefult to be able to gain observability about how the query load is
    distributed across different workloads.
    
    This is achieved with two new CLI flags, namely:
    
    * `enable-per-workload-table-metrics`: whether to enable or disable per
      workload metric collection - disabled by default to preserve the current
      behavior, thus making the new feature opt-in only.
    * `workload-label`: a string to look for in query comments to identify the
      workload running the current query.
    
    The workload is obtained by parsing query comments of the form:
    
    /* ... <workload_label>=<workload_name>; ... */
    
    For example, if vttablet is started with
    
    `--enable-per-workload-table-metrics --workload-label app_name`
    
    anda query is issued with a comment like
    
    /* ... app_name=shop; ... */
    
    then metrics will look like
    
    ```
    vttablet_query_counts{plan="Select",table="dual", workload="shop"} 15479
    ```
    
    instead of
    
    ```
    vttablet_query_counts{plan="Select",table="dual"} 15479
    ```
    
    Query comment parsing only takes place if `--enable-per-workload-table-metrics`
    is used, as to not incur parsing performance impact if the user does not want
    per workload metrics.
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * make linter happy
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * fix flags e2e test
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Address PR comments:
    
    * Obtain workload information on the vtgate instead of the vttablet, avoiding
      double parsing.
    * Treat workload name as a query directive.
    * Send workload name from vtgate to vttablet as ExecuteOptions.
    
    Additionally, annotate tabletserver's execution span with the workload name
    to also enrich traces with workload name data, in addition to metrics.
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * A few fixes:
    
    1. Rebuild some files with `make proto`.
    2. Protect against nil ExecuteOptions on the tabletserver.
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Fix flags e2e test
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Address PR comments
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Fixes
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Fix a comment
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Fix e2e flag test
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Update JS code for protobuf changes.
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Fix QueryEngine unit test
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Fix e2e flag test
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Fix spurious tab in comment
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    * Address PR comment
    
    Don't use dual format flag for new flags - stick with - separated ones.
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Eduardo J. Ortega U <5791035+ejortegau@users.noreply.github.com>

--- go/vt/vttablet/tabletserver/tabletserver.go
+++ go/vt/vttablet/tabletserver/tabletserver.go
@@ -746,6 +746,7 @@ func (tsv *TabletServer) execute(ctx context.Context, target *querypb.Target, sq
 				bindVariables = make(map[string]*querypb.BindVariable)
 			}
 			query, comments := sqlparser.SplitMarginComments(sql)
+
 			plan, err := tsv.qe.GetPlan(ctx, logStats, query, skipQueryPlanCache(options))
 			if err != nil {
 				return err
@@ -1403,6 +1404,7 @@ func (tsv *TabletServer) execRequest(
 	span, ctx := trace.NewSpan(ctx, "TabletServer."+requestName)
 	if options != nil {
 		span.Annotate("isolation-level", options.TransactionIsolation)
+		span.Annotate("workload_name", options.WorkloadName)
 	}
 	trace.AnnotateSQL(span, sqlparser.Preview(sql))
 	if target != nil {
@@ -1410,6 +1412,7 @@ func (tsv *TabletServer) execRequest(
 		span.Annotate("shard", target.Shard)
 		span.Annotate("keyspace", target.Keyspace)
 	}
+
 	defer span.Finish()
 
 	logStats := tabletenv.NewLogStats(ctx, requestName)
