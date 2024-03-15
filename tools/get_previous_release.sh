f98c567499283c08058c44dde939a3b0de03088b
Tim Vaillancourt <tim@timvaillancourt.com>
2024-03-15 18:03:57 +0100

    hardcode previous release
    Signed-off-by: Tim Vaillancourt <tim@timvaillancourt.com>

--- tools/get_previous_release.sh
+++ tools/get_previous_release.sh
@@ -20,25 +20,27 @@
 
 # github.base_ref $1
 
-target_release=""
+#target_release=""
+#
+#base_release_branch=$(echo "$1" | grep -E 'slack-vitess-r[0-9]*.0$')
+#if [ "$base_release_branch" == "" ]; then
+#  base_release_branch=$(echo "$2" | grep -E 'slack-vitess-r[0-9]*.0$')
+#fi
+#if [ "$base_release_branch" != "" ]; then
+#  major_release=$(echo "$base_release_branch" | sed 's/slack-vitess-r*//' | sed 's/\.0//')
+#  target_major_release=$((major_release-1))
+#  target_release_number=$(git show-ref --tags | grep -E 'refs/tags/v[0-9]*.[0-9]*.[0-9]*$' | sed 's/[a-z0-9]* refs\/tags\/v//' | awk -v FS=. -v RELEASE="$target_major_release" '{if ($1 == RELEASE) print; }' | sort -nr | head -n1)
+#  target_release="v$target_release_number"
+#else
+#  target_major_release=$(git show-ref | grep -E 'refs/remotes/origin/slack-vitess-r[0-9]*\.0$' | sed 's/[a-z0-9]* refs\/remotes\/origin\/slack-vitess-r//' | sed 's/\.0//' | sort -nr | head -n1)
+#  target_release_number=$(git show-ref --tags | grep -E 'refs/tags/v[0-9]*.[0-9]*.[0-9]*$' | sed 's/[a-z0-9]* refs\/tags\/v//' | awk -v FS=. -v RELEASE="$target_major_release" '{if ($1 == RELEASE) print; }' | sort -nr | head -n1)
+#  target_release="v$target_release_number"
+#  if [ -z "$target_release_number" ]
+#  then
+#    target_release="release-$target_major_release.0"
+#  fi
+#fi
+#
+#echo "$target_release"
 
-base_release_branch=$(echo "$1" | grep -E 'release-[0-9]*.0$')
-if [ "$base_release_branch" == "" ]; then
-  base_release_branch=$(echo "$2" | grep -E 'release-[0-9]*.0$')
-fi
-if [ "$base_release_branch" != "" ]; then
-  major_release=$(echo "$base_release_branch" | sed 's/release-*//' | sed 's/\.0//')
-  target_major_release=$((major_release-1))
-  target_release_number=$(git show-ref --tags | grep -E 'refs/tags/v[0-9]*.[0-9]*.[0-9]*$' | sed 's/[a-z0-9]* refs\/tags\/v//' | awk -v FS=. -v RELEASE="$target_major_release" '{if ($1 == RELEASE) print; }' | sort -nr | head -n1)
-  target_release="v$target_release_number"
-else
-  target_major_release=$(git show-ref | grep -E 'refs/remotes/origin/release-[0-9]*\.0$' | sed 's/[a-z0-9]* refs\/remotes\/origin\/release-//' | sed 's/\.0//' | sort -nr | head -n1)
-  target_release_number=$(git show-ref --tags | grep -E 'refs/tags/v[0-9]*.[0-9]*.[0-9]*$' | sed 's/[a-z0-9]* refs\/tags\/v//' | awk -v FS=. -v RELEASE="$target_major_release" '{if ($1 == RELEASE) print; }' | sort -nr | head -n1)
-  target_release="v$target_release_number"
-  if [ -z "$target_release_number" ]
-  then
-    target_release="release-$target_major_release.0"
-  fi
-fi
-
-echo "$target_release"
+echo slack-vitess-r14.0.5
