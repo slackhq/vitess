a8953eff0daaff8f2bd27d5265f309f0fb334b19
Tom Thornton <thomaswilliamthornton@gmail.com>
2025-10-24 15:02:17 -0500

    Tthornton slack 22.0 backports (#717)
    * VStream: Add flag to support copying only specific tables (#18184)
    
    Signed-off-by: Thomas Thornton <tthornton@salesforce.com>
    
    * Add a flag to vstream to exclude keyspace from table name (#18274)
    
    Signed-off-by: Thomas Thornton <tthornton@salesforce.com>
    
    * Fix flakey vstream metrics test (#18287)
    
    Signed-off-by: Thomas Thornton <tthornton@salesforce.com>
    
    * Add support for sending grpc server backend metrics via ORCA (#18282)
    
    Signed-off-by: Thomas Thornton <tthornton@salesforce.com>
    
    * VStream: Try new tablet on purged binlog error (#18710)
    
    Signed-off-by: twthorn <thomaswilliamthornton@gmail.com>
    
    * Run go mod tidy
    
    Signed-off-by: Thomas Thornton <tthornton@salesforce.com>
    
    * Run go fmt
    
    Signed-off-by: Thomas Thornton <tthornton@salesforce.com>
    
    * Regenerate proto files
    
    Signed-off-by: Thomas Thornton <tthornton@salesforce.com>
    
    * Fix sorting of --grpc-enable-orca-metrics flag
    
    Signed-off-by: Thomas Thornton <tthornton@salesforce.com>
    
    * Fix ordering of -grpc-enable-orca-metrics in vtcombo.txt
    
    Signed-off-by: Thomas Thornton <tthornton@salesforce.com>
    
    ---------
    
    Signed-off-by: Thomas Thornton <tthornton@salesforce.com>
    Signed-off-by: twthorn <thomaswilliamthornton@gmail.com>
    Co-authored-by: Noble Mittal <62551163+beingnoble03@users.noreply.github.com>
    Co-authored-by: Tanjin Xu <109303790+tanjinx@users.noreply.github.com>

--- go/vt/proto/vtgate/vtgate_vtproto.pb.go
+++ go/vt/proto/vtgate/vtgate_vtproto.pb.go
@@ -435,6 +435,12 @@ func (m *VStreamFlags) CloneVT() *VStreamFlags {
 	r.TabletOrder = m.TabletOrder
 	r.StreamKeyspaceHeartbeats = m.StreamKeyspaceHeartbeats
 	r.IncludeReshardJournalEvents = m.IncludeReshardJournalEvents
+	r.ExcludeKeyspaceFromTableName = m.ExcludeKeyspaceFromTableName
+	if rhs := m.TablesToCopy; rhs != nil {
+		tmpContainer := make([]string, len(rhs))
+		copy(tmpContainer, rhs)
+		r.TablesToCopy = tmpContainer
+	}
 	if len(m.unknownFields) > 0 {
 		r.unknownFields = make([]byte, len(m.unknownFields))
 		copy(r.unknownFields, m.unknownFields)
@@ -1841,6 +1847,25 @@ func (m *VStreamFlags) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
+	if m.ExcludeKeyspaceFromTableName {
+		i--
+		if m.ExcludeKeyspaceFromTableName {
+			dAtA[i] = 1
+		} else {
+			dAtA[i] = 0
+		}
+		i--
+		dAtA[i] = 0x50
+	}
+	if len(m.TablesToCopy) > 0 {
+		for iNdEx := len(m.TablesToCopy) - 1; iNdEx >= 0; iNdEx-- {
+			i -= len(m.TablesToCopy[iNdEx])
+			copy(dAtA[i:], m.TablesToCopy[iNdEx])
+			i = protohelpers.EncodeVarint(dAtA, i, uint64(len(m.TablesToCopy[iNdEx])))
+			i--
+			dAtA[i] = 0x4a
+		}
+	}
 	if m.IncludeReshardJournalEvents {
 		i--
 		if m.IncludeReshardJournalEvents {
@@ -2758,6 +2783,15 @@ func (m *VStreamFlags) SizeVT() (n int) {
 		n += 2
 	}
 	if m.IncludeReshardJournalEvents {
+		n += 2
+	}
+	if len(m.TablesToCopy) > 0 {
+		for _, s := range m.TablesToCopy {
+			l = len(s)
+			n += 1 + l + protohelpers.SizeOfVarint(uint64(l))
+		}
+	}
+	if m.ExcludeKeyspaceFromTableName {
 		n += 2
 	}
 	n += len(m.unknownFields)
@@ -6432,6 +6466,58 @@ func (m *VStreamFlags) UnmarshalVT(dAtA []byte) error {
 				}
 			}
 			m.IncludeReshardJournalEvents = bool(v != 0)
+		case 9:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field TablesToCopy", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.TablesToCopy = append(m.TablesToCopy, string(dAtA[iNdEx:postIndex]))
+			iNdEx = postIndex
+		case 10:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field ExcludeKeyspaceFromTableName", wireType)
+			}
+			var v int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				v |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			m.ExcludeKeyspaceFromTableName = bool(v != 0)
 		default:
 			iNdEx = preIndex
 			skippy, err := protohelpers.Skip(dAtA[iNdEx:])
